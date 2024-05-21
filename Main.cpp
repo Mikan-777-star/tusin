720e989f9147a4d48a421eb51e60732216c4959d
kuwatoro <mein777dedede@gmail.com>
2024-05-21 14:23:36 +0900

    modified:   Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -23,6 +23,12 @@ size_t handle_(char* readbuf,size_t readlen ,char** writebuf) {
     *writebuf = (char*)response.c_str();
     return response.size(); 
 }
+
+
+//簡単な使用方法
+//上のように「引数」と「戻り値」がhandle_clientと同じものを作成して
+//serverのコンストラクタに登録するだけ
+
 int main(int argc, char const *argv[])
 {
     server* s = new server(8080, handle_);
